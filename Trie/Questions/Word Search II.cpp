a785e3ca60b42d374ec43f1ddc5a4f2ae90010c9
gauravdatt2000 <gauravdatt2000@gmail.com>
2022-04-18 21:49:52 +0530

    TLE on 1 testcase optimization needed

--- Trie/Questions/Word Search II.cpp
+++ Trie/Questions/Word Search II.cpp
@@ -1,5 +1,39 @@
 // question : https://leetcode.com/problems/word-search-ii/
 
+
+/*
+
+edge case : TLE 
+
+12
+12
+aaaaaaaaaaaa
+aaaaaaaaaaaa
+aaaaaaaaaaaa
+aaaaaaaaaaaa
+aaaaaaaaaaaa
+aaaaaaaaaaaa
+aaaaaaaaaaaa
+aaaaaaaaaaaa
+aaaaaaaaaaaa
+aaaaaaaaaaaa
+aaaaaaaaaaaa
+aaaaaaaaaaaa
+10
+a
+aa
+aaa
+aaaa
+aaaaa
+aaaaaa
+aaaaaaa
+aaaaaaaa
+aaaaaaaaa
+aaaaaaaaaa
+
+
+*/
+
 #include"bits/stdc++.h"
 using namespace std ;
 
@@ -8,6 +42,9 @@ class  Node {
     map<char , Node* > Data ; 
     bool IsEnd = false ;
 };
+
+set<string> finalAns ;
+vector<pair<int , int>> dir{{1 , 0} , {-1 , 0} , {0 , 1} , {0 , -1} } ;
 
 class Trie{
     
@@ -65,8 +102,31 @@ class Trie{
 
     }
 
+    Node* getRoot(){
+        return root ;
+    }
+
 };
 
+void dfs(Node *tmp , vector<string> &vs , int x , int y , int &row , int &col , string ss , map<pair<int , int> , bool > isVisited ){
+
+    if(x>=0 and x<row and y>=0 and y<col and !isVisited[{x , y}] and tmp!=nullptr and tmp->Data.find(vs[x][y] )!=tmp->Data.end() ){
+        isVisited[{x , y}] = true ;
+        ss+= vs[x][y] ;
+        
+        if(tmp->Data[vs[x][y]]->IsEnd){
+            finalAns.insert( ss );
+        }
+        
+        tmp =  tmp->Data[vs[x][y]] ;
+
+        for(int i=0 ; i<4; i++){
+            dfs(tmp , vs , x+dir[i].first , y+dir[i].second , row , col , ss , isVisited) ;
+        }
+
+    }
+
+}
 
 
 int main(void){
@@ -96,9 +156,23 @@ int main(void){
 
         for(int j = 0 ; j<m ; j++){
 
+            map<pair<int , int> , bool > isVisited ;
+            Node *tmp_root = t1.getRoot() ;
+            dfs( tmp_root , vs , i , j , n , m , "" , isVisited ) ;
 
         }
 
     }
+
+    int len = finalAns.size() ;
+    auto p1 = finalAns.begin() ;
+    cout<<"[";
+    for(int i=0 ; i<len ; i++ ){
+        cout<< *p1;
+        if(i==len-1) continue ;
+        cout<<", ";
+        p1++;
+    }
+    cout<<"]";
 
 }
