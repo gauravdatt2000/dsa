c773763676f463737176b39af58b1cdfae6bcf4a
gauravdatt2000 <gauravdatt2000@gmail.com>
2022-09-13 02:13:52 +0530

    Added AddLast functionality

--- Linked_List/LinkedList.cpp
+++ Linked_List/LinkedList.cpp
@@ -1,8 +1,72 @@
 #include "bits/stdc++.h"
 using namespace std;
 
+class Node{
+  public : 
+  
+  int data;
+  Node *next ; 
+
+  Node(int val){
+    data = val;
+  }
+
+};
+
+class LinkedList{
+    public :
+
+    Node* head = nullptr ;
+    Node* tail =  nullptr ;
+
+    int size = 0;
+
+    void AddLast(int data){
+        size++;
+        Node *tmp = new Node(data) ;
+
+        if(head){
+            tail->next = tmp ;
+            tail = tail->next ;
+            return ;
+        }
+        head = tmp ;
+        tail = tmp ;
+    }
+
+    void Execute(){
+        
+        string ss;
+        int tmpData;
+
+        while (cin>>ss)
+        {
+            if(ss=="addLast"){
+                cin>>tmpData;
+                AddLast(tmpData);
+                continue;
+            }
+
+            return ;
+        }
+        
+    }
+
+    void Print(){
+        Node*tmp = head ;
+        while(tmp){
+            cout<<tmp->data<<endl;
+            tmp =  tmp->next ;
+        }
+    }
+};
+
 int main(){
-    string s1;
-    cin>>s1;
-    cout<<s1<<endl;
+    LinkedList linkedList ;
+    linkedList.Execute();
+    linkedList.Print();
+    cout<<linkedList.size<<endl;
+    if(linkedList.tail){
+        cout<<linkedList.tail->data<<endl;
+    }
 }
